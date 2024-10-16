4f14cc0a94aaa3422c333947d820fa6de6261890
Chandrashekharsk <cs9981625252@gmail.com>
2024-10-17 01:07:15 +0530

    init8

--- netflix/src/components/MainContainer.js
+++ netflix/src/components/MainContainer.js
@@ -1,20 +1,34 @@
-import React from 'react'
-import VideoTitle from './VideoTitle'
+import React, { useEffect, useState } from 'react';
+import VideoTitle from './VideoTitle';
 import VideoBackground from './VideoBackground';
-import {useSelector} from "react-redux";
+import { useSelector } from 'react-redux';
 
 const MainContainer = () => {
-  const movie = useSelector(store=>store.movie?.nowPlayingMovies);
-  if(!movie) return; // early return in react
- 
-  const {overview, id, title} = movie[4];
-  
+  const [movieIndex, setMovieIndex] = useState(0); 
+  const movie = useSelector((store) => store.movie?.nowPlayingMovies);
+
+  const selectedMovie = movie?.[movieIndex] || {};
+  const { overview, id, title } = selectedMovie;
+
+  useEffect(() => {
+    if (!movie || movie.length === 0) return;
+
+    // Interval to cycle through movies from 0 to movie.length - 1 and then start again from 0
+    const interval = setInterval(() => {
+      setMovieIndex((prevIndex) => (prevIndex + 1) % movie.length); 
+    }, 5000); // Change movie every 5 seconds
+
+    return () => clearInterval(interval); // Cleanup interval on component unmount
+  }, [movie]); 
+
+  if (!id || !title || !overview) return null;
+
   return (
     <div>
-        <VideoTitle title={title} overview={overview}/>
-        <VideoBackground movieId={id}/>
+      <VideoTitle title={title} overview={overview} />
+      <VideoBackground movieId={id} />
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
