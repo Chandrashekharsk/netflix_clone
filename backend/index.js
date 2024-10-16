890fae898eb0687860e25abe869c2ee108141709
Chandrashekharsk <cs9981625252@gmail.com>
2024-10-16 22:27:17 +0530

    edited backend cors

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,14 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:3000',
-    credentials:true
-}
-app.use(cors(corsOptions));
- 
+
+// const corsOptions = {
+//     origin:'https://netflix-3ppp.onrender.com/',
+//     credentials:true
+// }
+// app.use(cors(corsOptions));
+app.use(cors());
+
 // api
 app.use("/api/v1/user", userRoute);
 
